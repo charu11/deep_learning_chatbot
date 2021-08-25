4a60ce854b13691d4dc1ea7f881f87cc13c2af21
charun umesh <charunumesh@gmail.com>
2021-08-25 12:27:49 +0530

    buffering added

--- chatbot.py
+++ chatbot.py
@@ -15,5 +15,40 @@ def create_table():
     comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
 
+def format_data(data):
+    data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
+    return data
+
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{} LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result is not None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        return False
+
+
 if __name__ == '__main__':
     create_table()
+
+    row_counter = 0
+    paired_rows = 0
+
+    with open('data', buffering=1000) as f:
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            comment_id = row['name']
+            subreddit = row['subreddit']
+
+            parent_data = find_parent(parent_id)
+
